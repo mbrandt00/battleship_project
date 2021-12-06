389e56850791e02cd901c3627bbd6618cae855d2
Michael Brandt <msbrandt00@gmail.com>
2021-12-06 16:15:59 -0700

    Test that ship has a name

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,8 +2,16 @@ require './lib/ship'
 
 
 RSpec.describe Ship do
-  it 'it is an instance of Ship' do
-  cruiser = Ship.new("Cruiser", 3)
-  expect(cruiser).to be_instance_of(Ship)
+  before(:each) do
+    @cruiser = Ship.new("Cruiser", 3)
   end
+  it 'it is an instance of Ship' do
+  expect(@cruiser).to be_instance_of(Ship)
+  end
+
+  it 'has a name' do
+    expect(@cruiser.name).to eq('Cruiser')
+  end 
+
+
 end
