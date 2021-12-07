07aed9d9da85c62412f0c2fadecad8da84b47f05
Michael Brandt <msbrandt00@gmail.com>
2021-12-07 11:18:54 -0700

    Feature: add #ship and #empty method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,18 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate, :cell_status
 
   def initialize (coordinate)
     @coordinate = coordinate
+    @ship = @ship
   end
+
+  def ship
+    @ship
+  end
+
+  def empty?
+    true if @ship == nil
+  end 
+
 
 end
