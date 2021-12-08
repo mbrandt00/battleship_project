0650d1482edb45f50b3c49f0a0c341bb619d733f
Michael Brandt <msbrandt00@gmail.com>
2021-12-08 11:42:08 -0700

    Test: add initialization of class test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,4 +22,11 @@ RSpec.describe Board do
     @D2 = Cell.new("D2")
     @D3 = Cell.new("D3")
     @D4 = Cell.new("D4")
+    @board = Board.new()
   end
+
+  it 'is an instance of Board' do
+    expect(@board).to be_instance_of(Board)
+  end 
+
+end
