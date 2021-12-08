4f5bad8f4fc4d9a13e6521ce1a9efe6faf1234ec
Michael Brandt <msbrandt00@gmail.com>
2021-12-08 14:43:00 -0700

    Feature: add #consecutive method for horizontal array

--- lib/board.rb
+++ lib/board.rb
@@ -25,8 +25,24 @@ class Board
     cells.any? {|key, value| key == coordinate}
   end
 
+  def consecutive?(array_of_coordinates)
+    first_element = array_of_coordinates[0][0]
+    if array_of_coordinates.all? {|coordinate| first_element == coordinate[0]} #horizontal check
+      array_of_coordinates.map! {|coordinate| coordinate[1].to_i}
+      sorted_array = array_of_coordinates.sort
+      return (sorted_array.first..sorted_array.last).to_a == array_of_coordinates
+    # elsif #vertical check?
+    else
+      false
+    end
+  end
+
+
+
   def valid_placement?(ship, array_of_coordinates)
-    if array_of_coordinates.all? { |coordinate| self.valid_coordinate?(coordinate) } && ship.length <= array_of_coordinates.length
+    if ( array_of_coordinates.all? { |coordinate| self.valid_coordinate?(coordinate) } &&
+      ship.length <= array_of_coordinates.length &&
+    consecutive?)
       true
     else
       false
