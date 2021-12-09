19c41ff8db2b258155b62d668b094de7f5fca36b
Chris_K <ckjolhede@gmail.com>
2021-12-08 19:36:10 -0700

    refactor: added describe block for consecutive?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,11 +24,21 @@ RSpec.describe Board do
     expect(@board.valid_coordinate?('A1')).to be(true)
     expect(@board.valid_coordinate?('F7')).to be(false)
   end
+  
+  describe 'consecutive?' do
+    it 'will detect if array is consecutive' do
+      expect(@board.consecutive?(['A1', 'A2', 'A3'])).to be(true)
+      expect(@board.consecutive?(['A1', 'A3', 'A2'])).to be(false)
+      expect(@board.consecutive?(['B1', 'A2', 'A3'])).to be(false)
+    end
 
-  it 'will detect if array is consecutive' do
-    expect(@board.consecutive?(['A1', 'A2', 'A3'])).to be(true)
-    expect(@board.consecutive?(['A1', 'A3', 'A2'])).to be(false)
-    expect(@board.consecutive?(['B1', 'A2', 'A3'])).to be(false)
+    it 'will make sure coordinates are consecutive for vertical' do
+      expect(@board.consecutive?(['A1', 'B1', 'C1'])).to be(true)
+    end
+
+    it 'will make sure coordinates are consecutive for vertical in reverse' do
+      expect(@board.consecutive?(['C1', 'B1', 'A1'])).to be(true)
+    end
   end
 
   describe 'placement' do
@@ -47,22 +57,12 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@submarine, ['A1', 'A2'])).to be(true)
     end
 
-    it 'will make sure coordinates are consecutive for vertical' do
-      # binding.pry
-      expect(@board.consecutive?(['A1', 'B1', 'C1'])).to be(true)
-    end
-
-    it 'will make sure coordinates are consecutive for vertical in reverse' do
-      # binding.pry
-      expect(@board.consecutive?(['C1', 'B1', 'A1'])).to be(true)
-    end
 
     it 'will make sure ship is not placed diagonal' do
       expect(@board.valid_placement?(@cruiser, ['A1', 'B2', 'C3'])).to be(false)
       expect(@board.valid_placement?(@submarine, ['C2', 'D3'])).to be(false)
     end
   end
-
 
 
 
