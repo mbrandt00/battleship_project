7fcac7f830e272620a03e11d027acdae7e13bcdc
Michael Brandt <msbrandt00@gmail.com>
2021-12-12 14:45:01 -0700

    Feature: #place_ustom_ships and #main_menu

--- lib/runner.rb
+++ lib/runner.rb
@@ -5,7 +5,9 @@ require 'pry'
 
 
 class BattleShip
+  attr_accessor :board
   def initialize()
+    @board = board
   end
 
   def main_menu
@@ -13,25 +15,63 @@ class BattleShip
     puts "Enter 'p' to play. Enter 'q' to quit."
     input = gets.chomp
     if input.eql? 'p'
-      puts 'What dimensions would you like the board to be? Enter a number'
-      puts 'rows: '
-      rows = gets.chomp.to_i
-      puts 'columns: '
-      columns = gets.chomp.to_i
-      board = Board.new(rows, columns)
+      puts "How many rows would you like for your battleship board?"
+      rows = 0
+      while rows <=0
+        puts "please enter a number for the board's rows"
+        rows = gets.to_i #if string of letters converts to 0
+      end
+      puts "How many columns would you like for your battleship board?"
+      columns = 0
+      while columns <= 0
+        puts "please enter a number for the board's columns"
+        columns = gets.to_i
+      end
+      @board = Board.new(rows, columns)
       board.render()
 
 
     elsif input == 'q'
       puts 'goodbye'
+    end
     #else
     #  puts "Enter 'p' to play. Enter 'q' to quit."
-    end
   end
 
+    def place_custom_ships
+      puts "How many ships would you like to play with on your #{@board.rows} by #{@board.columns} board?"
+      ship_number = gets.chomp.to_i
+      ship_number.times do |ship|
+        puts "What would you like to call the #{ship + 1} ship?"
+        ship_name = gets.chomp
+        puts "How long would you like this ship to be?"
+        ship_length = 0
+        while ship_length <=0
+          puts 'please enter a number for the length of the ship'
+          ship_length = gets.to_i #if string of letters converts to 0
+        end
+        ship = Ship.new(ship_name, ship_length)
+        @board.custom_ships_array << ship
+        puts "Where would you like to place this ship? Enter coordinates (seperated by a space without quotes ie: A1 A2)"
+        coordinates = gets.chomp
+        array_of_coordinates = coordinates.split(' ')
+        until @board.valid_placement?(ship, array_of_coordinates) == true
+          puts "Make sure coordinates are consecutive, valid, don't already contain another ship, and is the same length as your ship"
+          coordinates = gets.chomp
+          array_of_coordinates = coordinates.split(' ')
+        end
+        @board.place(ship, array_of_coordinates)
+        @board.render(true)
+      end
+  end
 
+  def main_game()
+    main_menu
+    place_custom_ships
+    #computer places ship
+  end
 
 end
 
 game = BattleShip.new()
-game.main_menu
+game.main_game
