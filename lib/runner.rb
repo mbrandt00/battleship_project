b77e9125d2f40ea5f60f7f131c3b8aba396ee3e6
Michael Brandt <msbrandt00@gmail.com>
2021-12-14 11:19:06 -0700

    Feature: turn, end_game, who_won, refactor main_game

--- lib/runner.rb
+++ lib/runner.rb
@@ -14,72 +14,115 @@ class BattleShip
 
   def main_game()
     main_menu
-    puts "Would you like to play classic battleship (4x4 grid) with 2 ships or a dynamic game with customizable ships and board sizes?"
-    puts "Enter 'c' for classic, 'd' for dynamic"
     game_type = gets.chomp
     until (game_type.upcase == 'D' || game_type.upcase== 'C' ) do
       puts "Enter 'c' for classic, 'd' for dynamic"
       game_type = gets.chomp
     end
       if game_type.upcase.eql? 'D'
-        puts "How many rows would you like for your battleship board?"
-        rows = 0
-        while rows== 0 || rows > 26
-          puts "please enter a number for the board's rows between 0 and 26"
-          rows = gets.to_i #if string of letters converts to 0
-        end
-        puts "How many columns would you like for your battleship board?"
-        columns = 0
-        while columns== 0 || columns >26
-          puts "please enter a number for the board's columns between 0 and 26"
-          columns = gets.to_i
-        end
-        @board = Board.new(rows, columns)
-        @comp_board = Board.new(rows, columns)
-        place_custom_ships
+        dynamic_setup
       elsif game_type.upcase.eql? 'C'
-        @board = Board.new(4,4)
-        @comp_board = Board.new()
-        @board.render()
-        puts "Where would you like to place your 3 cell cruiser? Enter coordinates (seperated by a space without quotes ie: A1 A2)"
-        cruiser = Ship.new('cruiser', 3)
-        @comp_board.custom_ships_array << cruiser
-        coordinates = gets.chomp
-        array_of_coordinates = coordinates.split(' ')
-          until @board.valid_placement?(cruiser, array_of_coordinates) == true
-            puts "Make sure coordinates are consecutive, valid, don't already contain another ship, and are the same length as your ship (in this case #{cruiser.length} cells)."
-            # coordinates = gets.chomp
-            coordinates = 'A1 A2 A3'
-            # array_of_coordinates = coordinates.upcase.split(' ')
-            array_of_coordinates = coordinates.split(' ')
-        end
-      @board.place(cruiser, array_of_coordinates)
-      @board.render(true)
+        classic_setup
 
-
-      puts "Where would you like to place your 2 cell submarine? Enter coordinates (seperated by a space without quotes ie: A1 A2)"
-      submarine = Ship.new('submarine', 2)
-      @comp_board.custom_ships_array << submarine
-      coordinates = gets.chomp
-      array_of_coordinates = coordinates.split(' ')
-        until @board.valid_placement?(submarine, array_of_coordinates) == true
-          puts "Make sure coordinates are consecutive, valid, don't already contain another ship, and are the same length as your ship (in this case #{cruiser.length} cells)."
-          # coordinates = gets.chomp
-          coordinates = 'D1 D2'
-          # array_of_coordinates = coordinates.upcase.split(' ')
-          array_of_coordinates = coordinates.split(' ')
-      end
-      @board.place(submarine, array_of_coordinates)
-      @board.render(true)
-      place_computer_boards
     end
-  render_both_boards
+    place_computer_boards
+    system('clear') # only works on macs
+    render_both_boards #currently showing both computer ships
+    until who_won?
+      turn
+    end
+    end_game
+  end
+
+  def turn
+    puts "Please enter a single cell to fire upon (ex: A1 )"
+    selected_cell = gets.chomp
+    until @board.valid_coordinate?(selected_cell)
+      puts "Please enter a valid cell (ex: A1)."
+      selected_cell = gets.chomp
+    end
+    @comp_board.cells_hash[selected_cell].fire_upon
+    random_computer_shot = @board.cells_hash.keys.sample(1).join #join converts ['A1'] -> 'A1'
+    @board.cells_hash[random_computer_shot].fire_upon #smart shot would go here.
+    system('clear') #only works on macs.
+    render_both_boards
+  end
+
+  def end_game
+    puts "============= Computer's Board =============="
+    @comp_board.render(true) #for now
+    puts " "
+    puts "============= Your Board ================"
+    board.render(true)
+    who_won
   end
 
 
+  def who_won?
+    computer_won = @board.custom_ships_array.all? {|ship| ship.sunk?}
+    person_won = @comp_board.custom_ships_array.all? {|ship| ship.sunk?}
+    if computer_won || person_won
+      computer_won ? 'The computer was cheating with sonar and won' : 'Congratulations! You won!'
+    else
+      false
+    end
+  end
+
+
+  def dynamic_setup
+    puts "How many rows would you like for your battleship board?"
+    rows = 0
+    while rows== 0 || rows > 26
+      puts "please enter a number for the board's rows between 0 and 26"
+      rows = gets.to_i #if string of letters converts to 0
+    end
+    puts "How many columns would you like for your battleship board?"
+    columns = 0
+    while columns== 0 || columns >26
+      puts "please enter a number for the board's columns between 0 and 26"
+      columns = gets.to_i
+    end
+    @board = Board.new(rows, columns)
+    @comp_board = Board.new(rows, columns)
+    place_custom_ships
+  end
+
+  def classic_setup
+    @board = Board.new(4,4)
+    @comp_board = Board.new()
+    @board.render()
+    # puts "Where would you like to place your 3 cell cruiser? Enter coordinates (seperated by a space without quotes ie: A1 A2)"
+    cruiser = Ship.new('cruiser', 3)
+    @comp_board.custom_ships_array << cruiser
+    @board.custom_ships_array << cruiser
+    # coordinates = gets.chomp
+    coordinates = "A1 A2 A3"
+    array_of_coordinates = coordinates.split(' ')
+      until @board.valid_placement?(cruiser, array_of_coordinates) == true
+        puts "Make sure coordinates are consecutive, valid, don't already contain another ship, and are the same length as your ship (in this case #{cruiser.length} cells)."
+        coordinates = gets.chomp
+        array_of_coordinates = coordinates.split(' ')
+      end
+    @board.place(cruiser, array_of_coordinates)
+    @board.render(true)
+    # puts "Where would you like to place your 2 cell submarine? Enter coordinates (seperated by a space without quotes ie: A1 A2)"
+    submarine = Ship.new('submarine', 2)
+    @comp_board.custom_ships_array << submarine
+    @board.custom_ships_array << submarine
+    # coordinates = gets.chomp
+    coordinates = "D1 D2"
+    array_of_coordinates = coordinates.split(' ')
+      until @board.valid_placement?(submarine, array_of_coordinates) == true
+        puts "Make sure coordinates are consecutive, valid, don't already contain another ship, and are the same length as your ship (in this case #{cruiser.length} cells)."
+        coordinates = gets.chomp
+        array_of_coordinates = coordinates.upcase.split(' ')
+        array_of_coordinates = coordinates.split(' ')
+      end
+      @board.place(submarine, array_of_coordinates)
+  end
+
   def place_computer_boards
     @comp_board.poss_ship_placements # computer ships
-    print @comp_board.ship_placements
     @comp_board.custom_ships_array.each do |ship|
     random_sample = @comp_board.ship_placements[ship].sample(1).first
       while @comp_board.valid_placement?(ship, random_sample) == false
@@ -122,7 +165,8 @@ class BattleShip
     puts "Enter 'p' to play. Enter 'q' to quit."
     input = gets.chomp
     if input.capitalize.eql? 'P'
-      puts 'Initializing Game!'
+      puts "Would you like to play classic battleship (a 4x4 grid with 2 set ships) or a dynamic game with customizable ships and board sizes?"
+      puts "Enter 'c' for classic, 'd' for dynamic"
     elsif input.capitalize.eql? 'Q'
       puts 'goodbye'
       exit
@@ -135,7 +179,7 @@ class BattleShip
 
   def render_both_boards
     puts "============= Computer's Board =============="
-    @comp_board.render(true)
+    @comp_board.render() #for now
     puts " "
     puts "============= Your Board ================"
     board.render(true)
@@ -145,3 +189,15 @@ end
 
 game = BattleShip.new()
 game.main_game
+
+# -------- Current Bugs----------------------
+# still to do modify ship_length to not exceed board limits
+# not fire upon cell that is already fired upon
+# sunk? needs fixing. If (2) is fixed it should resolve. Right now sunk? is true if health == 0
+# clear for windows?
+
+# --------Ideas-----------
+
+# You sunk ship.name...
+# play again?
+# Smart computer?
